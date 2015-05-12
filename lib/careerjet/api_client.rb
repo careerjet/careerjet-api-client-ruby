3fc8e9e2aa709da12a0fe718db84ad438fda5899
Careerjet <thomas@careerjet.com>
2015-05-12 13:18:48 +0100

    Added Careerjet::APIClient

---
+++ lib/careerjet/api_client.rb
@@ -0,0 +1,19 @@
+require 'careerjet/constants'
+require 'careerjet/errors'
+require 'careerjet/client'
+require 'careerjet/mash'
+require 'careerjet/version'
+
+class Careerjet::APIClient
+
+  attr_accessor :careerjet_client
+
+  def initialize(params)
+    @careerjet_client = Careerjet::Client.new(params) 
+  end    
+
+  def search(search_params)
+    results = @careerjet_client.search(search_params)
+    Careerjet::Mash.from_json results
+  end
+end
